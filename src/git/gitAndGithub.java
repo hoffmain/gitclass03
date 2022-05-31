3b1b323a4c587a6ff8c800823fb37509b7bb7ff4
hoffmain <elexia.hoffman@gmail.com>
2022-05-31 18:54:19 -0500

    1st commit

--- src/git/gitAndGithub.java
+++ src/git/gitAndGithub.java
@@ -1,5 +1,11 @@
 package git;
 
 public class gitAndGithub {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("today we learn github");
+		System.out.println("today is a very good day");
+	}
 
 }
